980b90409fdbfb8fe701f7ac1ecc79d22bce9164
Heli Devine <heli.devine@gmail.com>
2023-05-16 16:15:50 +0100

    added CORS

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 
 **Full stack application with Java Springboot backend and React frontend**
 
+To run this application navigate to the directory in the terminal window and type
+
+http://localhost:8080/swagger-ui/index.html
+
+`mvn spring-boot:run`
+
+`mvn run`
+
 **Project status**
 
 under construction
--- src/main/java/journeys/JourneysApplication.java
+++ src/main/java/journeys/JourneysApplication.java
@@ -2,6 +2,9 @@ package journeys;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class JourneysApplication {
@@ -10,4 +13,15 @@ public class JourneysApplication {
 		SpringApplication.run(JourneysApplication.class, args);
 	}
 
-}
+	@Bean
+	public WebMvcConfigurer webMvcConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry corsRegistry) {
+				corsRegistry.addMapping("/**").allowedOrigins("*");
+			}
+		};
+	}
+
+	}
+
