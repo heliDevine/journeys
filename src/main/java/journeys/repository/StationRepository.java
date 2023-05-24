d6a399a24e50d2545078ddc59ad749f77c74b9eb
Heli Devine <heli.devine@gmail.com>
2023-05-24 10:23:44 +0100

    added bike capacity info

--- src/main/java/journeys/repository/StationRepository.java
+++ src/main/java/journeys/repository/StationRepository.java
@@ -20,7 +20,7 @@ public interface StationRepository extends MongoRepository<Station, ObjectId> {
     @Query(value = "{}", fields = "{FID:0, nimi: 0, namn:0, osoite: 0, city: 0, stad:0, operator:0, capacity:0, x:0, y:0 }")
     List<Station> findAll();
 
-    @Query(value = "{}", fields = "{FID:0, nimi: 0, namn:0, address: 0, city: 0, stad:0, operator:0, capacity:0, x:0, y:0 }")
+    @Query(value = "{}", fields = "{FID:0, nimi: 0, namn:0, address: 0, city: 0, stad:0, operator:0, x:0, y:0 }")
     Page<Station> findAllStationsWithFilteredFields(Pageable pageable);
 
 }
