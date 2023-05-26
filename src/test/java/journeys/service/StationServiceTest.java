c5b1c8fbb2438264097d64676cc62f490c8521b1
Heli Devine <heli.devine@gmail.com>
2023-05-26 15:01:46 +0100

    refactored StationService tests

--- src/test/java/journeys/service/StationServiceTest.java
+++ src/test/java/journeys/service/StationServiceTest.java
@@ -1,6 +1,5 @@
 package journeys.service;
 
-import journeys.model.Journey;
 import journeys.model.Station;
 import journeys.repository.JourneyRepository;
 import journeys.repository.StationRepository;
@@ -55,26 +54,21 @@ class StationServiceTest {
 
     @Test
     void itFetchesStationById() {
-        String id = "123";
-        Station station = Station.builder()
-                .id(id)
-                .stationNameEN("Piccadilly Circus")
-                .build();
+
+        Station station = createStation();
+
         when(stationRepository.findById("123")).thenReturn(station);
         Station actualStation = stationService.getStationByID("123");
 
-        assertThat(actualStation.getStationNameEN()).isEqualTo("Piccadilly Circus");
+        assertThat(actualStation.getStationNameEN()).isEqualTo("Kallio");
     }
 
     @Test
     void itDoesntReturnNonExistingStation() {
 
         String id = "123";
-        Station station = Station.builder()
-                .id(id)
-                .stationNameEN("Piccadilly Circus")
-                .build();
-        when(stationRepository.findById("123")).thenReturn(null);
+
+        when(stationRepository.findById(id)).thenReturn(null);
         Station actualStation = stationService.getStationByID("123");
 
         assertThat(actualStation).isNull();
@@ -82,26 +76,18 @@ class StationServiceTest {
 
     @Test
     void itFetchesStationByName() {
-        String id = "123";
-        Station station = Station.builder()
-                .id(id)
-                .stationNameEN("Piccadilly Circus")
-                .build();
-        when(stationRepository.findByStationNameEN("Piccadilly Circus")).thenReturn(station);
 
-        Station actualStation = stationService.getStationsByNameEN("Piccadilly Circus");
+        Station station = createStation();
+
+        when(stationRepository.findByStationNameEN(station.getStationNameEN())).thenReturn(station);
+
+        Station actualStation = stationService.getStationsByNameEN("Kallio");
         assertThat(actualStation.getId()).isEqualTo("123");
-
     }
 
     @Test
     void itDoesntReturnNonExistingStationByName() {
 
-        String id = "123";
-        Station station = Station.builder()
-                .id(id)
-                .stationNameEN("Piccadilly Circus")
-                .build();
         when(stationRepository.findByStationNameEN("Piccadilly Circus")).thenReturn(null);
         Station actualStation = stationService.getStationsByNameEN("Piccadilly Circus");
 
@@ -111,42 +97,41 @@ class StationServiceTest {
     @Test
     void itReturnsTotalDistanceFromStation() {
 
-        Station station = Station.builder()
-                .id("123")
-                .StationID(1)
-                .stationNameEN("Kallio")
-                .build();
+        Station station = createStation();
+
         when(journeyRepository.calculateTotalJourneyDistanceFromStation(station.getStationID()))
                 .thenReturn(1500.0);
 
         Double totalDistance = stationService.totalJourneyDistance(station);
         assertThat(totalDistance).isEqualTo(1500.0);
-
     }
 
     @Test
     void itReturnsTotalCountOfDepartingJourneys() {
 
-        Station station = Station.builder()
-                .id("123")
-                .StationID(1)
-                .stationNameEN("Kallio")
-                .totalReturnedJourneys(3)
-                .totalDepartingJourneys(2)
-                .totalJourneyDistanceFromStation(1500)
-                .build();
+        Station station = createStation();
+
         when(journeyRepository.countByDepartureStationId(station.getStationID()))
                 .thenReturn(station.getTotalDepartingJourneys());
 
         Long totalCount = stationService.totalJourneyCountDeparted(station);
         assertThat(totalCount).isEqualTo(2);
-
     }
 
     @Test
     void itReturnsTotalCountOfReturningJourneys() {
 
-        Station station = Station.builder()
+        Station station = createStation();
+
+        when(journeyRepository.countByReturnStationId(station.getStationID()))
+                .thenReturn(station.getTotalReturnedJourneys());
+
+        Long totalCount = stationService.totalJourneyCountReturned(station);
+        assertThat(totalCount).isEqualTo(3);
+    }
+
+    private Station  createStation() {
+        return Station.builder()
                 .id("123")
                 .StationID(1)
                 .stationNameEN("Kallio")
@@ -154,36 +139,5 @@ class StationServiceTest {
                 .totalDepartingJourneys(2)
                 .totalJourneyDistanceFromStation(1500)
                 .build();
-        when(journeyRepository.countByReturnStationId(station.getStationID()))
-                .thenReturn(station.getTotalReturnedJourneys());
-
-        Long totalCount = stationService.totalJourneyCountReturned(station);
-        assertThat(totalCount).isEqualTo(3);
-
-    }
-
-    private List<Journey> createJourneys() {
-        List<Journey> journeys;
-        journeys = Arrays.asList(
-                Journey.builder()
-                        .id("123")
-                        .departureStationId(1)
-                        .departureStationName("Kallio")
-                        .distance(1000)
-                        .build(),
-                Journey.builder()
-                        .id("456")
-                        .departureStationId(2)
-                        .departureStationName("Manchester")
-                        .distance(2000)
-                        .build(),
-                Journey.builder()
-                        .id("789")
-                        .departureStationId(1)
-                        .departureStationName("Kallio")
-                        .distance(500)
-                        .build()
-        );
-        return journeys;
     }
 }
